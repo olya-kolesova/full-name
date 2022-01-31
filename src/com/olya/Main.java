7fc41d572e350e57651ad33e11d1a30cfca3739a
Olga Kolesova <olka.kolesova@gmail.com>
2022-01-31 20:08:28 +0700

    second commit

---
+++ src/com/olya/Main.java
@@ -0,0 +1,33 @@
+package com.olya;
+
+import java.util.Objects;
+import java.util.Scanner;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Insert the number of person id from 1 to 12, please.");
+        Scanner scanner = new Scanner(System.in);
+        String idNum = scanner.next();
+	    String url = "https://reqres.in/api/users?id=" + idNum;
+
+        String json = Parser.getJSON(url);
+
+        try {
+            JSONObject obj = new JSONObject(Objects.requireNonNull(json));
+            String lastName1 = obj.getJSONObject("data").getString("last_name");
+            String firstName1 = obj.getJSONObject("data").getString("first_name");
+            Person person = new Person(firstName1, lastName1);
+            System.out.println(person.getFirstName() + " " + person.getLastName());
+
+        } catch (JSONException e) {
+            System.out.println("Error on parse data in JSONParser.java");
+        } catch (NullPointerException e) {
+            System.out.println("Null pointer");
+        }
+
+    }
+}
---
+++ src/com/olya/Parser.java
@@ -0,0 +1,55 @@
+package com.olya;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class Parser {
+    private String urlAddress;
+
+    Parser(String urlAddress) {
+        this.urlAddress = urlAddress;
+    }
+
+    public static String getJSON(String urlAddress) {
+        HttpsURLConnection con = null;
+        try {
+            URL u = new URL(urlAddress);
+            con = (HttpsURLConnection) u.openConnection();
+
+            con.connect();
+
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            br.close();
+            return sb.toString();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("User not found!");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+}
